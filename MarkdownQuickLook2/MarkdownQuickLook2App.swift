23402771254c6aad71baca91d08acd0da5b004d1
Noah Scott <noahsscott@gmail.com>
2026-01-08 18:14:02 +0800

    Initial Commit

---
+++ MarkdownQuickLook2/MarkdownQuickLook2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownQuickLook2App.swift
+//  MarkdownQuickLook2
+//
+//  Created by Noah Scott on 8/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownQuickLook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownQuickLook2Tests/MarkdownQuickLook2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MarkdownQuickLook2Tests.swift
+//  MarkdownQuickLook2Tests
+//
+//  Created by Noah Scott on 8/1/2026.
+//
+
+import Testing
+
+struct MarkdownQuickLook2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
